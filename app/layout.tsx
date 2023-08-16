30c17eb322a042ca91e81bfe9d43d625d6081879
Muzaffer Kılıçaslan <thepitohui@gmail.com>
2023-08-16 12:23:35 +0300

    created next app, created homepage and navbar, made connection with admin app to get categories into the navbar

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import { Urbanist } from 'next/font/google'
+
+import type { Metadata } from 'next'
+
+
+import Footer from '@/components/footer'
+import Navbar from "@/components/navbar"
+
+import './globals.css'
+
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Store',
+  description: 'Store',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        <Footer />  
+      </body>
+    </html>
+  )
+}
