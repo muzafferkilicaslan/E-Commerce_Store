2597e59a5310ad7b6c75231829fd3cad0227b720
Muzaffer Kılıçaslan <thepitohui@gmail.com>
2024-03-26 01:40:32 +0300

    feat: created cart page

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,35 +1,38 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import { ShoppingBag } from "lucide-react"
+import React, { useEffect, useState } from "react";
+import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-import Button from "@/components/ui/button"
-import useCart from "@/hooks/use-cart"
+import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const cart = useCart();
+  const router = useRouter();
 
-    const [isMounted,setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    const cart = useCart();
-
-    useEffect(()=>{
-        setIsMounted(true);
-    },[])
-
-    if(!isMounted){
-        return null;
-    }
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-        <Button className="flex items-center rounded-full bg-black px-4 py-2">
-            <ShoppingBag size={20} color="white" />
-            <span className="ml-2 text-sm font-medium text-white">
-                {cart.items.length}
-            </span>
-        </Button>
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full bg-black px-4 py-2"
+      >
+        <ShoppingBag size={20} color="white" />
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default NavbarActions
+export default NavbarActions;
