b6ab9846e3fd9072ae559d7d2860be6b52738bf3
Muzaffer Kılıçaslan <thepitohui@gmail.com>
2024-03-25 23:14:59 +0300

    feat: added add to cart functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from 'next'
 import Footer from '@/components/footer'
 import Navbar from "@/components/navbar"
 import ModalProvider from "@/providers/modal-provider"
+import ToastProvider from "@/providers/toast-provider"
 
 import './globals.css'
 
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />  
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -9,6 +9,7 @@ import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
   data: Product;
@@ -17,15 +18,22 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
   const router = useRouter();
   const previewModal = usePreviewModal();
+  const cart = useCart();
+
   const handleClick = () => {
     router.push(`/products/${data?.id}`);
   };
 
   const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
-    console.log("onPreview")
     event.stopPropagation();
     previewModal.onOpen(data);
   };
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    console.log("add to cart calisti.")
+    event.stopPropagation();
+    cart.addItem(data);
+  }; 
 
   return (
     <div
@@ -46,7 +54,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
